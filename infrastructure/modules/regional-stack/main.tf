bdc84907c7f2dd354f3e9385b3759e99b5b6c5f4
William Lam <williamntlam@gmail.com>
2025-12-29 16:49:29 -0500

    feat: added IAM infrastructure with role + policies for s3 bucket and sqs queue

--- infrastructure/modules/regional-stack/main.tf
+++ infrastructure/modules/regional-stack/main.tf
@@ -26,3 +26,51 @@ resource "aws_sqs_queue" "transaction_queue" {
         Purpose = "TransactionQueue"
     }
 }
+
+resource "aws_iam_role" "ledger_app_role" {
+    name = "${var.region}-ledge-app-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            }
+        }]
+    })
+}
+
+resource "aws_iam_role_policy" "ledger_app_policy" {
+    name = "${var.region}-ledger-app-policy"
+    role = aws_iam_role.ledger_app_role.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Action = [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:ListBucket"
+            ]
+            Resource = [
+                aws_s3_bucket.audit_logs.arn,
+                "${aws_s3_bucket.audit_logs.arn}/*"
+            ]
+        },
+        {
+            Effect = "Allow"
+            Action = [
+                "sqs:SendMessage",
+                "sqs:ReceiveMessage",
+                "sqs:DeleteMessage",
+                "sqs:GetQueueAttributes"
+            ]
+            Resource = [
+                aws_sqs_queue.transaction_queue.arn
+            ]
+        }]
+    })
+}
