94ce6641952f58b8ebafffd444f0d4d5cf023d76
William Lam <williamntlam@gmail.com>
2025-12-29 16:43:22 -0500

    feat: added code to provision for aws sqs queue for message queue

--- infrastructure/modules/regional-stack/main.tf
+++ infrastructure/modules/regional-stack/main.tf
@@ -14,3 +14,15 @@ resource "aws_s3_bucket_versioning" "audit_logs" {
         status = "Enabled"
     }
 }
+
+resource "aws_sqs_queue" "transaction_queue" {
+    name = "${var.region}-transaction-queue"
+
+    visibility_timeout_seconds = 30
+    message_retention_seconds = 1209600
+
+    tags = {
+        Region = var.region
+        Purpose = "TransactionQueue"
+    }
+}
